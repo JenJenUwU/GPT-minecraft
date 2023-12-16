f6c932b2573cce280eb4ebe63e517e35b9f2735c
Jay <jenjentw358@gmail.com>
2023-12-16 18:13:03 -0500

    init mod

---
+++ src/main/java/com/jenjenuwu/gptminecraft/GPTMinecraftClient.java
@@ -0,0 +1,8 @@
+package com.jenjenuwu.gptminecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+public class GPTMinecraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
